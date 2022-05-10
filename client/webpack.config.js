677203c6714cbc85b3afbda4b5911a8e98734631
Julia <julia.gray30@gmail.com>
2022-05-10 16:04:05 +0100

    configured workbox plugin for manifest file

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -2,9 +2,7 @@ const HtmlWebpackPlugin = require("html-webpack-plugin");
 const WebpackPwaManifest = require("webpack-pwa-manifest");
 const path = require("path");
 const { InjectManifest } = require("workbox-webpack-plugin");
-const WorkBoxPlugIn = require("workbox-webpack-plugin")
-
-// TODO: Add and configure workbox plugins for a service worker and manifest file.
+const WorkBoxPlugIn = require("workbox-webpack-plugin");
 
 module.exports = () => {
   return {
@@ -25,11 +23,27 @@ module.exports = () => {
         template: "./index.html",
         title: "Webpack Plugin",
       }),
+      new WebpackPwaManifest({
+        name: "JATE",
+        short_name: "JATE",
+        description: "A simple text editor",
+        background_color: "#7eb4e2",
+        theme_color: "#7eb4e2",
+        start_url: "/",
+        publicPath: "/",
+        icons: [
+          {
+            src: path.resolve("src/images/logo.png"),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join("src", "icons"),
+          },
+        ],
+      }),
       new InjectManifest({
-        swSrc: './src/sw.js',
-      swDest: 'install.js',
-        }),
-        new WorkBoxPlugIn.GenerateSW()
+        swSrc: "./src/sw.js",
+        swDest: "install.js",
+      }),
+      new WorkBoxPlugIn.GenerateSW(),
     ],
 
     module: {
